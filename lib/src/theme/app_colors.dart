456379bce6b55cc9bdb9d548492d31c7eb7c1b8a
Damir <125348732+ki55el@users.noreply.github.com>
2024-07-05 04:25:34 +0600

    feat: added image and theme

---
+++ lib/src/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+sealed class AppColors {
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const lightBlue = Colors.lightBlue;
+}
